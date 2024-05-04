e720384edde83a9adf243c3bcba442a8a31a26ba
tableau ethereal <145373380+tabethereal@users.noreply.github.com>
2024-05-05 02:31:23 +0800

    update player

--- ggc4ryToSjUe32f05c13/js/script.js
+++ ggc4ryToSjUe32f05c13/js/script.js
@@ -1,4 +1,22 @@
-//TODO: 歌词、歌单、操作按钮合并、真随机、倍速、初始声音
+//TODO: 歌词、歌单、真随机、歌名搜索、小窗音量
+
+/**
+ * 全局变量
+ */
+let currentMusic = 0;
+let lastPlayedMusic = 0;
+
+const playModeType = {
+  CYCLE: 0,
+  REPEAT: 1,
+  SHUFFLE: 2
+};
+let playMode = playModeType.CYCLE;
+
+let playRate = 1;
+
+let shuffleList = [];
+
 
 /**
  * 为元素批量添加监听事件
@@ -52,61 +70,47 @@ const togglePlaylist = function () {
 }
 addEventOnElements(playlistTogglers, "click", togglePlaylist);
 
-
-
 /**
- * PLAYLIST ITEM
- * 
- * remove active state from last time played music
- * and add active state in clicked music
+ * 为当前音乐添加播放高亮，取消上一首音乐的播放高亮
  */
-
 const playlistItems = document.querySelectorAll("[data-playlist-item]");
-
-let currentMusic = 0;
-let lastPlayedMusic = 0;
-
-const changePlaylistItem = function () {
+const highlightMusic = function () {
   playlistItems[lastPlayedMusic].classList.remove("playing");
   playlistItems[currentMusic].classList.add("playing");
+  playlist.children[currentMusic].scrollIntoView({ behavior: 'smooth', block: 'nearest' });
 }
 
+/**
+ * 展示音乐的标题和所属歌单，设置音源
+ */
+const playerTitle = document.querySelector("[data-title]");
+const playerCollection = document.querySelector("[data-collection]");
+const audioSource = new Audio(musicData[currentMusic].musicPath);
+const loadMusic = function () {
+  playerTitle.textContent = musicData[currentMusic].title;
+  playerCollection.textContent = musicData[currentMusic].collection;
+  audioSource.src = musicData[currentMusic].musicPath;
+  audioSource.addEventListener("loadeddata", updateDuration);
+}
+
+/**
+ * 切换音乐并播放
+ * 鼠标单击音乐时，执行此动作
+ */
+const switchMusic = function () {
+  highlightMusic();
+  loadMusic();
+  playMusic();
+}
 addEventOnElements(playlistItems, "click", function () {
   lastPlayedMusic = currentMusic;
   currentMusic = Number(this.dataset.playlistItem);
-  changePlaylistItem();
+  switchMusic();
 });
 
-
-
 /**
- * PLAYER
- * 
- * change all visual information on player, based on current music
+ * 将时间值转为时间显示文本
  */
-
-const playerTitle = document.querySelector("[data-title]");
-const playerCollection = document.querySelector("[data-collection]");
-
-const audioSource = new Audio(musicData[currentMusic].musicPath);
-
-const changePlayerInfo = function () {
-  playerTitle.textContent = musicData[currentMusic].title;
-  playerCollection.textContent = musicData[currentMusic].collection;
-
-  audioSource.src = musicData[currentMusic].musicPath;
-
-  audioSource.addEventListener("loadeddata", updateDuration);
-  playMusic();
-}
-
-addEventOnElements(playlistItems, "click", changePlayerInfo);
-
-/** update player duration */
-const playerDuration = document.querySelector("[data-duration]");
-const playerSeekRange = document.querySelector("[data-seek]");
-
-/** pass seconds and get timcode formate */
 const getTimecode = function (duration) {
   const minutes = Math.floor(duration / 60);
   const seconds = Math.ceil(duration - (minutes * 60));
@@ -114,53 +118,48 @@ const getTimecode = function (duration) {
   return timecode;
 }
 
+/**
+ * 音源加载后执行：设置进度条的最大值和显示时间最大值
+ */
+const playerDuration = document.querySelector("[data-duration]");
+const playerSeekRange = document.querySelector("[data-seek]");
 const updateDuration = function () {
   playerSeekRange.max = Math.ceil(audioSource.duration);
   playerDuration.textContent = getTimecode(Number(playerSeekRange.max));
 }
 
-audioSource.addEventListener("loadeddata", updateDuration);
-
-
-
 /**
- * PLAY MUSIC
- * 
- * play and pause music when click on play button
+ * 播放按钮：切换播放与暂停
  */
-
 const playBtn = document.querySelector("[data-play-btn]");
-
-let playInterval;
-
+let playTimer;
 const playMusic = function () {
   if (audioSource.paused) {
     audioSource.play();
     playBtn.classList.add("active");
-    playInterval = setInterval(updateRunningTime, 500);
+    playTimer = setInterval(updateRunningTime, 500);
   } else {
     audioSource.pause();
     playBtn.classList.remove("active");
-    clearInterval(playInterval);
+    clearInterval(playTimer);
   }
 }
-
 playBtn.addEventListener("click", playMusic);
 
-
-/** update running time while playing music */
-
+/**
+ * 播放时周期执行：更新进度条的数值和显示时间数值，检查是否播放结束
+ */
 const playerRunningTime = document.querySelector("[data-running-time");
-
 const updateRunningTime = function () {
+  if (audioSource.ended) {
+    playBtn.classList.remove("active");
+    audioSource.currentTime = 0;
+    skipNext();
+  }
   playerSeekRange.value = audioSource.currentTime;
   playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-
   updateRangeFill();
-  isMusicEnd();
 }
-
-
 
 /**
  * 更新滑动条的视觉效果，包括进度条和音量条
@@ -174,127 +173,105 @@ const updateRangeFill = function () {
 }
 addEventOnElements(ranges, "input", updateRangeFill);
 
-
-
 /**
- * SEEK MUSIC
- * 
- * seek music while changing player seek range
+ * 拨动进度条时设置进度
  */
-
 const seek = function () {
   audioSource.currentTime = playerSeekRange.value;
   playerRunningTime.textContent = getTimecode(playerSeekRange.value);
 }
-
 playerSeekRange.addEventListener("input", seek);
 
-
-
 /**
- * END MUSIC
+ * 下一首
  */
-
-const isMusicEnd = function () {
-  if (audioSource.ended) {
-    playBtn.classList.remove("active");
-    audioSource.currentTime = 0;
-    playerSeekRange.value = audioSource.currentTime;
-    playerRunningTime.textContent = getTimecode(audioSource.currentTime);
-    updateRangeFill();
-    skipNext();
-  }
-}
-
-
-
-/**
- * SKIP TO NEXT MUSIC
- */
-
 const playerSkipNextBtn = document.querySelector("[data-skip-next]");
-
 const skipNext = function () {
   lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
+  if (playMode == playModeType.SHUFFLE) {
+    if (shuffleList.length == 0) { createShuffleList(); }
+    currentMusic = shuffleList[(shuffleList.indexOf(currentMusic) + 1) % shuffleList.length];
+  } else if (playMode == playModeType.REPEAT) {
   } else {
-    currentMusic >= musicData.length - 1 ? currentMusic = 0 : currentMusic++;
+    currentMusic = (currentMusic + 1) % musicData.length;
   }
-
-  changePlayerInfo();
-  changePlaylistItem();
+  switchMusic();
 }
-
 playerSkipNextBtn.addEventListener("click", skipNext);
 
-
-
 /**
- * SKIP TO PREVIOUS MUSIC
+ * 上一首
  */
-
 const playerSkipPrevBtn = document.querySelector("[data-skip-prev]");
-
 const skipPrev = function () {
   lastPlayedMusic = currentMusic;
-
-  if (isShuffled) {
-    shuffleMusic();
+  if (playMode == playModeType.SHUFFLE) {
+    if (shuffleList.length == 0) { createShuffleList(); }
+    currentMusic = shuffleList[(shuffleList.indexOf(currentMusic) - 1 + shuffleList.length) % shuffleList.length];
+  } else if (playMode == playModeType.REPEAT) {
   } else {
-    currentMusic <= 0 ? currentMusic = musicData.length - 1 : currentMusic--;
+    currentMusic = (currentMusic - 1 + musicData.length) % musicData.length;
   }
-
-  changePlayerInfo();
-  changePlaylistItem();
+  switchMusic();
 }
-
 playerSkipPrevBtn.addEventListener("click", skipPrev);
 
-
-
 /**
- * SHUFFLE MUSIC
+ * 创建随机音乐下标列表
  */
-
-/** get random number for shuffle */
-const getRandomMusic = () => Math.floor(Math.random() * musicData.length);
-
-const shuffleMusic = () => currentMusic = getRandomMusic();
-
-const playerShuffleBtn = document.querySelector("[data-shuffle]");
-let isShuffled = false;
-
-const shuffle = function () {
-  playerShuffleBtn.classList.toggle("active");
-
-  isShuffled = isShuffled ? false : true;
+const shuffleArray = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
-
-playerShuffleBtn.addEventListener("click", shuffle);
-
-
+const createShuffleList = () => shuffleList = shuffleArray([...Array(musicData.length).keys()]);
 
 /**
- * REPEAT MUSIC
+ * 播放模式按钮：切换列表循环、单曲循环、随机播放
  */
-
-const playerRepeatBtn = document.querySelector("[data-repeat]");
-
-const repeat = function () {
-  if (!audioSource.loop) {
-    audioSource.loop = true;
+const playerModeBtn = document.querySelector("[data-play-mode]");
+const playerModeBtnActive = playerModeBtn.querySelector("[data-play-mode-active]");
+const changeMode = function () {
+  playMode = (playMode + 1) % 3;
+  this.classList.remove("active");
+  audioSource.loop = false;
+  if (playMode == playModeType.CYCLE) {
+  } else if (playMode == playModeType.REPEAT) {
     this.classList.add("active");
-  } else {
-    audioSource.loop = false;
-    this.classList.remove("active");
+    audioSource.loop = true;
+    playerModeBtnActive.textContent = "repeat_one";
+  } else if (playMode == playModeType.SHUFFLE) {
+    this.classList.add("active");
+    playerModeBtnActive.textContent = "shuffle";
   }
 }
+playerModeBtn.addEventListener("click", changeMode);
 
-playerRepeatBtn.addEventListener("click", repeat);
-
-
+/**
+ * 播放速率切换
+ */
+const playerRateBtn = document.querySelector("[data-play-rate]");
+const playerRateBtnActive = playerRateBtn.querySelector("[data-play-rate-active]");
+const changeRate = function () {
+  if (playRate == 0.5) {
+    playRate = 1;
+  } else if (playRate == 1) {
+    playRate = 1.5;
+    playerRateBtnActive.textContent = "speed_1_5x";
+  } else if (playRate == 1.5) {
+    playRate = 2;
+    playerRateBtnActive.textContent = "speed_2x";
+  } else if (playRate == 2) {
+    playRate = 0.5;
+    playerRateBtnActive.textContent = "speed_0_5x";
+  }
+  playRate == 1 ? this.classList.remove("active") : this.classList.add("active");
+  audioSource.playbackRate = playRate;
+  audioSource.defaultPlaybackRate = playRate;
+}
+playerRateBtn.addEventListener("click", changeRate);
 
 /**
  * 调整音量
@@ -304,7 +281,9 @@ const playerVolumeBtn = document.querySelector("[data-volume-btn]");
 const changeVolume = function () {
   audioSource.volume = playerVolumeRange.value;
   audioSource.muted = false;
-  if (audioSource.volume <= 0.1) {
+  if (audioSource.volume == 0) {
+    playerVolumeBtn.children[0].textContent = "volume_off";
+  } else if (audioSource.volume <= 0.1) {
     playerVolumeBtn.children[0].textContent = "volume_mute";
   } else if (audioSource.volume <= 0.5) {
     playerVolumeBtn.children[0].textContent = "volume_down";
@@ -326,3 +305,14 @@ const muteVolume = function () {
   }
 }
 playerVolumeBtn.addEventListener("click", muteVolume);
+
+
+/**
+ * 初始化
+ */
+loadMusic();
+
+playerVolumeRange.value = 0.5;
+playerVolumeRange.dispatchEvent(new Event("input"));
+changeVolume();
+
